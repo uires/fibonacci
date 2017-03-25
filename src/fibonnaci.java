df089076de24f40d5c4dfba18d6b1c7956e1236e
uires deivide <uiresdeivide1@gmail.com>
2017-03-24 21:36:28 -0300

    Função Fibonacci maneira normal e maneira recursiva

---
+++ src/fibonnaci.java
@@ -0,0 +1,35 @@
+
+public class fibonnaci {
+	
+	static void imprime(int numero){
+		
+		int atual = 0;
+		int anterior = 0;
+		
+		for (int i = 0; i < numero; i++) {
+			if(i == 1){
+				atual = 1;
+				anterior = 0;
+			} else {
+				atual += anterior;
+				anterior = atual - anterior;
+			}
+			System.out.print(atual + " ");
+		}
+	}
+	
+	static int imprimeRecursivo(int numero){
+		if(numero <= 1){
+			return numero;
+		} else {
+			return imprimeRecursivo(numero -1)+imprimeRecursivo(numero-2);
+		}
+	}
+	
+	public static void main(String[] args) {
+		//imprime(45);
+		
+		System.out.println(imprimeRecursivo(5));
+	}
+	
+}
